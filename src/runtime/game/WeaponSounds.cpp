bea4770ef919b212665a2a83b8b00f03a6789d6b
7ddf-1 <l7n7002@gmail.com>
2024-11-23 13:39:25 +0400

    Update WeaponSounds.cpp

--- src/runtime/game/WeaponSounds.cpp
+++ src/runtime/game/WeaponSounds.cpp
@@ -3,17 +3,72 @@
 #include "../unity/Unity.hpp"
 #include "../util/HookingUtil.hpp"
 #include "../framework/UIManager.hpp"
+#include "../framework/bool.hpp"
 
 namespace WeaponSounds
 {
-	bool killAura = false;
 
 	$Hook(void, WeaponSoundsUpdate, (Il2CppObject* _this))
 	{
-		if (killAura)
+
+		if (bools::SilentAim)
 		{
 			Field<bool>(_this, "isRoundMelee") = true;
 			Field<float>(_this, "radiusRoundMelee") = 99999.0f;
+		}
+		else
+		{
+			Field<bool>(_this, "isRoundMelee") = false;
+			Field<float>(_this, "radiusRoundMelee") = 0.0f;
+		}
+
+
+		if (bools::ExplosiveBullet)
+		{
+			Field<bool>(_this, "bulletExplode") = true;
+		}
+		else
+		{
+			Field<bool>(_this, "bulletExplode") = false;
+		}
+
+		
+		if (bools::bazooka)
+		{
+			Field<bool>(_this, "bazooka") = true;
+		}
+		else
+		{
+			Field<bool>(_this, "bazooka") = false;
+		}
+
+
+		if (bools::InfiniteAmmo)
+		{
+			//Field<>(_this, "unknown") = 999;
+		}
+		else
+		{
+			//Field<>(_this, "unknown") = 1;
+		}
+
+
+		if (bools::freezer)
+		{
+			Field<bool>(_this, "freezer") = true;
+		}
+		else
+		{
+			Field<bool>(_this, "freezer") = false;
+		}
+
+		if (bools::railgun)
+		{
+			Field<bool>(_this, "railgun") = true;
+		}
+		else
+		{
+			Field<bool>(_this, "railgun") = false;
 		}
 
 		$CallOrig(WeaponSoundsUpdate, _this);
@@ -21,7 +76,13 @@ namespace WeaponSounds
 
 	void UIUpdate()
 	{
-		ImGui::Checkbox("Killaura", &killAura);
+		ImGui::Checkbox("Silent Aim", &bools::SilentAim);
+		ImGui::Checkbox("Infinite Ammo", &bools::InfiniteAmmo);
+		ImGui::Checkbox("Explosive Bullet", &bools::ExplosiveBullet);
+		ImGui::Checkbox("Rpg Bullet", &bools::bazooka);
+		ImGui::Checkbox("Freezer", &bools::freezer);
+		ImGui::Checkbox("Rail Gun", &bools::railgun);
+
 	}
 
 	void INIT()
